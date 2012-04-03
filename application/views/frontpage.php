8066d718692b4f1d9ed8b03b0944d433b3360ce9
Steven Lu <tacticalazn@gmail.com>
2012-04-03 13:29:01 -0400

    Added more content to frontpage.

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -1,6 +1,7 @@
 <div class="container">
-   <div class="well">
-      <h3>CodeIgniter Boostrap</h3>
-      <p>Contains useful utilties</p>
+   <div class="hero-unit">
+      <h2>CodeIgniter Bootstrap</h2>
+      <p>CodeIgniter Bootstrap kick starts the development process of the web development process by including Twitter Bootstrap into CodeIgniter. It also includes certain libraries such as AWS and Facebook in-case you are developing applications requiring those SDKs. So stop writing the same code over again and start working on your idea.</p>
+      <a class="btn btn-primary btn-large" href="https://github.com/sjlu/CodeIgniter-Bootstrap">View on Github</a>
    </div>
 </div>
