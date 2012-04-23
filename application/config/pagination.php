9eb6d2889993eef409f5bee0b468927634134b32
Steven Lu <tacticalazn@gmail.com>
2012-04-23 11:30:20 -0400

    Adding pagination config to use twitter bootstrap.

---
+++ application/config/pagination.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * Enclosure markup.
+ * The tags which wrap the ENTIRE pagination.
+ */
+$config['full_tag_open'] = '<div class="pagination"><ul>';
+$config['full_tag_close'] = '</ul></div>';
+
+/*
+ * All other page number markup.
+ */
+$config['num_tag_open'] = '<li>';
+$config['num_tag_close'] = '</li>';
+
+/*
+ * The current "page" tag markup.
+ */
+$config['cur_tag_open'] = '<li class="active"><a>';
+$config['cur_tag_close'] = '</a></li>';
+
+/*
+ * The text for the next and last links.
+ */
+//$config['next_link'] = '&rarr;';
+//$config['prev_link'] = '&larr;';
+$config['next_link'] = false;
+$config['prev_link'] = false;
+
+/*
+ * Do not edit, bypass CI bug.
+ */
+$config['uri_segment'] = $this->uri->total_segments();
+
+/*
+ * The "First" and "Last" segments.
+ */
+$config['first_tag_open'] = $config['num_tag_open'];
+$config['first_tag_close'] = $config['num_tag_close'];
+$config['first_link'] = '&laquo;';
+
+$config['last_tag_open'] = $config['num_tag_open'];
+$config['last_tag_close'] = $config['num_tag_close'];
+$config['last_link'] = '&raquo;';
