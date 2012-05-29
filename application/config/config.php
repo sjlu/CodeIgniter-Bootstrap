b0ac8d19576153761ab439815adff1280c2e2889
Steven Lu <tacticalazn@gmail.com>
2012-05-29 01:14:49 -0400

    Removing default encryption key.

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'ZaFEceXaswegadruj63rAhuxaPhaguca';
+$config['encryption_key'] = '';
 
 /*
 |--------------------------------------------------------------------------
