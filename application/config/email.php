e43342c1729b485e7e8de3706bd292f6693c06f1
Steven Lu <tacticalazn@gmail.com>
2012-05-29 01:16:12 -0400

    Commenting out heroku auto-detect.

--- application/config/email.php
+++ application/config/email.php
@@ -19,16 +19,12 @@ $config['smtp_user'] = '';
 $config['smtp_pass'] = '';
 
 /*
- * Heroku Sendgrid auto-detection.
- * If smtp_host is written, it will override 
- * Heroku environment variables.
+ * Heroku Sendgrid information.
  */
-if (isset($_SERVER['SENDGRID_USERNAME']) && empty($config['smtp_host']))
-{
-   $config['protocol'] = 'smtp';
-   $config['smtp_host'] = 'smtp.sendgrid.net';
-   $config['smtp_port'] = 587;
-   $config['smtp_user'] = $_SERVER['SENDGRID_USERNAME'];
-   $config['smtp_pass'] = $_SERVER['SENDGRID_PASSWORD'];
-}
-
+/*
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'smtp.sendgrid.net';
+$config['smtp_port'] = 587;
+$config['smtp_user'] = $_SERVER['SENDGRID_USERNAME'];
+$config['smtp_pass'] = $_SERVER['SENDGRID_PASSWORD'];
+*/
