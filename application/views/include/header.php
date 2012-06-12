28ed71a52f656cf8d2f4d779d0cf73ec0263e438
Steven Lu <tacticalazn@gmail.com>
2012-06-12 15:11:02 -0400

    Adding font-awesome. Load CSS before JS.

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -9,11 +9,14 @@
 
    <title>CodeIgniter Bootstrap</title>
 
-   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-   <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
-
    <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+	<link href="<?= base_url('assets/css/font-awesome.css') ?>" rel="stylesheet">
    <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
    <link href="<?= base_url('assets/css/custom.css') ?>" rel="stylesheet">
+
+   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+   <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+
 </head>
 <body>
