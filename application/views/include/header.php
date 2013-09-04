708be52bfa7fc911410e0af9ece08b582a476ac5
Topsitemakers <support@topsitemakers.com>
2013-09-04 15:27:11 +0200

    updating lodash to 1.3.1

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -15,7 +15,7 @@
 
    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-   <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.2.1/lodash.min.js"></script>
+   <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.3.1/lodash.min.js"></script>
    <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
    <script src="<?php echo base_url('assets/js/custom.js') ?>"></script>
 </head>
