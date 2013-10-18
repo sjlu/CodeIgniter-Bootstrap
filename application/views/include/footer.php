067d3847d2d42ef1abc232f931e24a625f4d9a30
Steven Lu <tacticalazn@gmail.com>
2013-10-18 14:38:01 -0400

    Putting all the JS back to the footer.

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,2 +1,7 @@
+   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+   <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+   <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.2.1/lodash.min.js"></script>
+   <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+   <script src="<?php echo base_url('assets/js/custom.js') ?>"></script>
 </body>
 </html>
