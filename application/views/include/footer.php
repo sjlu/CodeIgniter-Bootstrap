e156fe70dfdb37f449c5830a37c13e24b0f0d863
Renan Martins <renan@ead.cesumar.br>
2013-10-18 15:02:27 -0300

    javascript removing the header, to improve the performace

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,2 +1,10 @@
+
+   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+   <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/1.3.1/lodash.min.js"></script>
+   <?php foreach ($js as $script): ?>
+   <script src="<?php echo base_url( $script ) ?>"></script>
+   <?php endforeach; ?>
+
 </body>
 </html>
