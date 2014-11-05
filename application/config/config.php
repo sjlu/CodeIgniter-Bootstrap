a6ced6839e28a49327c3ab08116504b089e014a7
John Paul Medina <medina.jpaul@gmail.com>
2014-11-05 18:15:34 -0500

    Updated 2.2 config to have CDN and Native Autoload Function

--- application/config/config.php
+++ application/config/config.php
@@ -34,24 +34,6 @@ $config['cdn_url']	= '';
 /*
 
 /*
-| -------------------------------------------------------------------
-|  Native Auto-load
-| -------------------------------------------------------------------
-| 
-| Nothing to do with config/autoload.php, this allows PHP autoload to work
-| for base controllers and some third-party libraries.
-|
-*/
-function __autoload($class)
-{
-	if(strpos($class, 'CI_') !== 0)
- 	{
-  		@include_once( APPPATH . 'core/'. $class . EXT );
- 	}
-}
-/*
-
-/*
 |--------------------------------------------------------------------------
 | Index File
 |--------------------------------------------------------------------------
@@ -391,6 +373,25 @@ $config['rewrite_short_tags'] = FALSE;
 |
 */
 $config['proxy_ips'] = '';
+
+
+/*
+| -------------------------------------------------------------------
+|  Native Auto-load
+| -------------------------------------------------------------------
+| 
+| Nothing to do with config/autoload.php, this allows PHP autoload to work
+| for base controllers and some third-party libraries.
+|
+*/
+function __autoload($class)
+{
+	if(strpos($class, 'CI_') !== 0)
+ 	{
+  		@include_once( APPPATH . 'core/'. $class . EXT );
+ 	}
+}
+/*
 
 
 /* End of file config.php */
