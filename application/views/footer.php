e3c6209a8121b2634642b00da0d53822654fba10
Steven Lu <tacticalazn@gmail.com>
2014-11-06 11:24:07 -0500

    Rewriting initial views.

---
+++ application/views/footer.php
@@ -0,0 +1,3 @@
+  <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" context="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <title>CodeIgniter Bootstrap</title>
+    <meta name="description" content="">
+
+    <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/font-awesome.min.css') ?>" rel="stylesheet">
+</head>
+<body>
