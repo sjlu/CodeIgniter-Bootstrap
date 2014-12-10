6e1ca3c6612e2769a804c6d2c22b0778c9fd9034
Marcelo Diniz <marcelo.leo27@gmail.com>
2014-12-10 14:14:49 -0200

    change folder assets for better maintainability and create folder includes for views header and footer

---
+++ application/views/include/footer.php
@@ -0,0 +1,4 @@
+	<script src="<?php echo base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
+	<script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.min.js') ?>"></script>
+</body>
+</html>
--- application/views/include/header.php
+++ application/views/include/header.php
@@ -8,7 +8,7 @@
     <title>CodeIgniter Bootstrap</title>
     <meta name="description" content="">
 
-    <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?php echo base_url('assets/css/font-awesome.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/plugins/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/plugins/font-awesome/css/font-awesome.min.css') ?>" rel="stylesheet">
 </head>
 <body>
