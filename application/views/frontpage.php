09a9089c75d8ffc03f73707ab041ef5dc8f66178
John Paul Medina <medina.jpaul@gmail.com>
2015-02-27 22:59:35 -0500

    Base

--- application/views/frontpage.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-   <div class="jumbotron">
-      <h2>CodeIgniter Bootstrap</h2>
-      <p>CodeIgniter Bootstrap kick starts the development process of the web development process by including Twitter Bootstrap into CodeIgniter. It also includes certain libraries such as AWS and Facebook in-case you are developing applications requiring those SDKs. So stop writing the same code over again and start working on your idea.</p>
-      <a class="btn btn-primary btn-large" href="https://github.com/sjlu/CodeIgniter-Bootstrap"> <i class="fa fa-github fa-lg"></i> View on Github</a>
-   </div>
-</div>
